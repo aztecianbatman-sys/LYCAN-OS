9cb51d2d55b193c1eaa579c3ee28d48531bbb574
aztecianbatman-sys <aztecianbatman@gmail.com>
2026-09-03 17:14:01 +0530

    LYCAN #6 add real uninstall lifecycle API

--- src/package/package_manager.h
+++ src/package/package_manager.h
@@ -1,36 +1,20 @@
 #pragma once
 #include "core/lyfs.h"
+#include "core/process.h"
 #include "core/security.h"
 #include <filesystem>
 #include <string>
 #include <vector>
-
 namespace lycan {
-struct Package {
-    std::string id,name,version,publisher,description,sha256,entry;
-    std::vector<std::string> permissions;
-    std::filesystem::path source;
-    std::string installLocation;
-    std::string installTime;
-    std::string previousVersion;
-};
-
+struct Package { std::string id,name,version,publisher,description,sha256,entry; std::vector<std::string> permissions; std::filesystem::path source; std::string installLocation,installTime,previousVersion; };
 class PackageManager {
 public:
-    PackageManager(Lyfs& fs, SecurityPolicy& security);
-    bool install(const Package& pkg);
-    bool installArchive(const std::filesystem::path& package, const std::string& expectedSha256, std::string* error = nullptr);
-    bool installArchiveFromCatalog(const std::filesystem::path& package, const std::string& catalogJson, std::string* error = nullptr);
-    static std::string packageSha256(const std::filesystem::path& package);
-    static bool verifySha256(const std::string& calculated, const std::string& expected);
-    static std::string catalogSha256(const std::string& catalogJson, const std::string& id, const std::string& version);
-    bool inspectArchive(const std::filesystem::path& package, Package& pkg, std::string* error = nullptr) const;
-    bool uninstall(const std::string& id);
-    bool rollback(const std::string& id);
-    std::vector<Package> installed() const;
-    static bool validId(const std::string& id);
+ PackageManager(Lyfs& fs, SecurityPolicy& security, ProcessManager* processes = nullptr);
+ bool install(const Package& pkg); bool installArchive(const std::filesystem::path&,const std::string&,std::string* error=nullptr); bool installArchiveFromCatalog(const std::filesystem::path&,const std::string&,std::string* error=nullptr);
+ static std::string packageSha256(const std::filesystem::path&); static bool verifySha256(const std::string&,const std::string&); static std::string catalogSha256(const std::string&,const std::string&,const std::string&);
+ bool inspectArchive(const std::filesystem::path&,Package&,std::string* error=nullptr) const; bool uninstall(const std::string& id); bool rollback(const std::string& id); std::vector<Package> installed() const; static bool validId(const std::string&);
 private:
-    Lyfs& fs_; SecurityPolicy& security_; std::filesystem::path db_;
-    bool recordInstall(const Package& pkg, const std::string& checksum, const std::string& previousVersion);
+ Lyfs& fs_; SecurityPolicy& security_; ProcessManager* processes_; std::filesystem::path db_,launcherDb_;
+ bool recordInstall(const Package&,const std::string&,const std::string&); bool registerLauncher(const Package&); bool removeLauncher(const std::string&);
 };
 }
