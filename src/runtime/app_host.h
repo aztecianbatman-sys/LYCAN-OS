8fc9b705fb4883beefb53b3dc670ef8f6fe63c18
aztecianbatman-sys <aztecianbatman@gmail.com>
2026-09-03 17:45:34 +0530

    LYCAN #13: integrate settings and Store client into AppHost

--- src/runtime/app_host.h
+++ src/runtime/app_host.h
@@ -3,9 +3,11 @@
 #include "core/lyfs.h"
 #include "core/process.h"
 #include "core/security.h"
-#include "package/package_manager.h"
 #include "core/snapshot.h"
+#include "core/settings.h"
+#include "package/package_manager.h"
 #include "runtime/app_manager.h"
+#include "store/store_client.h"
 #include <filesystem>
 #include <string>
 namespace lycan {
@@ -14,9 +16,9 @@ public:
     AppHost(std::filesystem::path dataRoot);
     void boot();
     std::string execute(const std::string& command);
-    AresVm& vm(); Lyfs& fs(); ProcessManager& processes(); SecurityPolicy& security(); PackageManager& packages(); SnapshotManager& snapshots(); ApplicationManager& apps();
+    AresVm& vm(); Lyfs& fs(); ProcessManager& processes(); SecurityPolicy& security(); PackageManager& packages(); SnapshotManager& snapshots(); ApplicationManager& apps(); SettingsManager& settings(); StoreClient& store();
 private:
     std::filesystem::path root_;
-    AresVm vm_; Lyfs fs_; ProcessManager processes_; SecurityPolicy security_; PackageManager packages_; SnapshotManager snapshots_; ApplicationManager applications_;
+    AresVm vm_; Lyfs fs_; ProcessManager processes_; SecurityPolicy security_; PackageManager packages_; SnapshotManager snapshots_; SettingsManager settings_; ApplicationManager applications_; StoreClient store_;
 };
 }
