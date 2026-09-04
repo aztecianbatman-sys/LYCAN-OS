3c7eb4e9a6293818893af09503822aa65a0267d4
aztecian batman <aztecianbatman@gmail.com>
2026-09-04 14:03:37 +0530

    fix: wire pure Electron runtime IPC

--- frontend/preload.js
+++ frontend/preload.js
@@ -2,8 +2,9 @@ const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld('lycan', {
   command: command => ipcRenderer.invoke('lycan:command', command),
   window: action => ipcRenderer.send('lycan:window', String(action || '')),
-  gecko: url => ipcRenderer.invoke('lycan:gecko', url),
-  installPackage: () => ipcRenderer.invoke('lycan:install-lypkg'),
+  gecko: url => ipcRenderer.invoke('lycan:open-gecko', url),
+  installPackage: () => ipcRenderer.invoke('lycan:install-package'),
   listPackages: () => ipcRenderer.invoke('lycan:list-packages'),
+  downloadPackage: url => ipcRenderer.invoke('lycan:download-package', String(url || '')),
   launchPackage: id => ipcRenderer.invoke('lycan:launch-package', String(id || ''))
 });
