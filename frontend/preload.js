d081e7d2c7f5793094ad55c6f2878c59d863c05a
aztecianbatman-sys <aztecianbatman@gmail.com>
2026-09-04 10:33:41 +0530

    Add native package registry and launcher bridge

--- frontend/preload.js
+++ frontend/preload.js
@@ -3,5 +3,7 @@ contextBridge.exposeInMainWorld('lycan', {
   command: command => ipcRenderer.invoke('lycan:command', command),
   window: action => ipcRenderer.send('lycan:window', String(action || '')),
   gecko: url => ipcRenderer.invoke('lycan:gecko', url),
-  installPackage: () => ipcRenderer.invoke('lycan:install-lypkg')
+  installPackage: () => ipcRenderer.invoke('lycan:install-lypkg'),
+  listPackages: () => ipcRenderer.invoke('lycan:list-packages'),
+  launchPackage: id => ipcRenderer.invoke('lycan:launch-package', String(id || ''))
 });
