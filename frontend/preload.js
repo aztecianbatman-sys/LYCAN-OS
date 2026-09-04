73ffdc9fe65b2e644c5380b48646f64abceac518
aztecianbatman-sys <aztecianbatman@gmail.com>
2026-09-04 10:04:25 +0530

    LYCAN OS: expose isolated Gecko and package bridges

--- frontend/preload.js
+++ frontend/preload.js
@@ -1,5 +1,7 @@
 const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld('lycan', {
   command: command => ipcRenderer.invoke('lycan:command', command),
-  window: action => ipcRenderer.send('lycan:window', String(action || ''))
+  window: action => ipcRenderer.send('lycan:window', String(action || '')),
+  gecko: url => ipcRenderer.invoke('lycan:gecko', url),
+  installPackage: () => ipcRenderer.invoke('lycan:install-lypkg')
 });
