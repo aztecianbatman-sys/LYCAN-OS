fa1d6f70e235e90af0e0c18a638d93402b382bce
aztecian batman <aztecianbatman@gmail.com>
2026-09-04 14:05:13 +0530

    fix: preserve Gecko bridge compatibility

--- frontend/preload.js
+++ frontend/preload.js
@@ -3,6 +3,7 @@ contextBridge.exposeInMainWorld('lycan', {
   command: command => ipcRenderer.invoke('lycan:command', command),
   window: action => ipcRenderer.send('lycan:window', String(action || '')),
   gecko: url => ipcRenderer.invoke('lycan:open-gecko', url),
+  openGecko: url => ipcRenderer.invoke('lycan:open-gecko', url),
   installPackage: () => ipcRenderer.invoke('lycan:install-package'),
   listPackages: () => ipcRenderer.invoke('lycan:list-packages'),
   downloadPackage: url => ipcRenderer.invoke('lycan:download-package', String(url || '')),
